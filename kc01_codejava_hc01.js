fc9669bb9585c009be77dfc97e4d8aa79327a1a3
h-ch-n <44386805+h-ch-n@users.noreply.github.com>
2018-11-01 00:59:35 -0400

    Add files via upload

---
+++ kc01_codejava_hc01.js
@@ -0,0 +1,58 @@
+function newWindow1() {
+    
+    "use strict";
+    
+    setTimeout(window.open("popup3.html", "_blank", "left=5,top=500,width=675,height=900", "toolbar=no,menubar=no,titlebar=no", "fullscreen=no", "location=no", "status=no", "scrollbars=no", "resizable=no"), 15000);
+    
+    setTimeout(window.open("video1.html", "_blank", "left=60,top=100,width=560,height=315", "toolbar=no,menubar=no,titlebar=no"), 10000);
+    
+    setTimeout(window.open("popup2.html", "_blank", "left=150,top=300,width=640,height=427", "toolbar=no,menubar=no,titlebar=no", "fullscreen=no", "location=no", "status=no", "scrollbars=no", "resizable=no"), 5000);
+    
+    setTimeout(window.open("popup4.html", "_blank", "left=1000,top=100,width=506,height=506", "toolbar=no,menubar=no,titlebar=no", "fullscreen= no", "location =no", "status=no", "scrollbars=no", "resizable=no"), 9000);
+    
+    setTimeout(window.open("popup6.html", "_blank", "left=1300,top=900,width=900,height=633", "toolbar=no,menubar=no,titlebar=no", "fullscreen=no", "location=no", "status=no", "scrollbars=no", "resizable=no"), 1000);
+    
+    setTimeout(window.open("popup5.html", "_blank", "left=600,top=450,width=200,height=200", "toolbar=no,menubar=no,titlebar=no", "fullscreen=no", "location=no", "status=no", "scrollbars=no", "resizable=no"), 20000);
+}
+
+function newWindow2() {
+    
+    "use strict";
+    
+    setTimeout(window.open("popup7.html", "_blank", "left=25,top=500,width=900,height=600", "toolbar=no,menubar=no,titlebar=no", "fullscreen=no", "location=no", "status=no", "scrollbars=no", "resizable=no"), 15000);
+    
+    setTimeout(window.open("popup8.html", "_blank", "left=600,top=450,width=560,height=315", "toolbar=no,menubar=no,titlebar=no"), 15000);
+    
+    setTimeout(window.open("popup9.html", "_blank", "left=15,top=300,width=640,height=427", "toolbar=no,menubar=no,titlebar=no", "fullscreen=no", "location=no", "status=no", "scrollbars=no", "resizable=no"), 5000);
+    
+    setTimeout(window.open("popup10.html", "_blank", "left=810,top=10,width=750,height=442", "toolbar=no,menubar=no,titlebar=no", "fullscreen= no", "location =no", "status=no", "scrollbars=no", "resizable=no"), 7000);
+    
+    setTimeout(window.open("popup11.html", "_blank", "left=1000,top=100,width=900,height=647", "toolbar=no,menubar=no,titlebar=no", "fullscreen= no", "location =no", "status=no", "scrollbars=no", "resizable=no"), 9000);
+    
+}
+
+function newWindow3() {
+    
+    "use strict";
+    
+    setTimeout(window.open("popup12.html", "_blank", "left=5,top=500,width=600,height=338", "toolbar=no,menubar=no,titlebar=no", "fullscreen=no", "location=no", "status=no", "scrollbars=no", "resizable=no"), 15000);
+    
+    setTimeout(window.open("popup13.html", "_blank", "left=1000,top=100,width=360,height=270", "toolbar=no,menubar=no,titlebar=no"), 2000);
+    
+    setTimeout(window.open("popup14.html", "_blank", "left=150,top=300,width=900,height=898", "toolbar=no,menubar=no,titlebar=no", "fullscreen=no", "location=no", "status=no", "scrollbars=no", "resizable=no"), 5000);
+    
+    setTimeout(window.open("popup15.html", "_blank", "left=60,top=100,width=1024,height=768", "toolbar=no,menubar=no,titlebar=no", "fullscreen= no", "location =no", "status=no", "scrollbars=no", "resizable=no"), 9000);
+    
+}
+
+function newWindow4() {
+    
+    "use strict";
+    
+    setTimeout(window.open("popup17.html", "_blank", "left=60,top=100,width=700,height=465", "toolbar=no,menubar=no,titlebar=no"), 20000);
+    
+    setTimeout(window.open("popup18.html", "_blank", "left=600,top=300,width=640,height=640", "toolbar=no,menubar=no,titlebar=no", "fullscreen=no", "location=no", "status=no", "scrollbars=no", "resizable=no"), 5000);
+    
+    setTimeout(window.open("popup16.html", "_blank", "left=200,top=500,width=200,height=200", "toolbar=no,menubar=no,titlebar=no", "fullscreen=no", "location=no", "status=no", "scrollbars=no", "resizable=no"), 15000);
+    
+}
